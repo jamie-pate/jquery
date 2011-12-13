2ecd1a300c1caff78b6a4c41117f59e55af7537a
Jamie Pate <jamie.pate@gmail.com>
2011-12-12 16:54:37 -0800

    Added detection of css3 box-sizing:border-box to getWidthHeight in order to detect the correct size for that model

--- src/css.js
+++ src/css.js
@@ -244,24 +244,27 @@ function getWidthOrHeight( elem, name, extra ) {
 	// Start with offset property
 	var val = name === "width" ? elem.offsetWidth : elem.offsetHeight,
 		i = name === "width" ? 1 : 0,
-		len = 4;
+		len = 4,
+		boxSizing = jQuery([ "-ms-", "-o-","-moz-","-webkit-", "" ]).is( function( i, pre ) {
+			return jQuery.css( elem, pre + "box-sizing" ) === "border-box";
+		});
 
-	if ( val > 0 ) {
-		if ( extra !== "border" ) {
-			for ( ; i < len; i += 2 ) {
-				if ( !extra ) {
-					val -= parseFloat( jQuery.css( elem, "padding" + cssExpand[ i ] ) ) || 0;
-				}
-				if ( extra === "margin" ) {
-					val += parseFloat( jQuery.css( elem, extra + cssExpand[ i ] ) ) || 0;
-				} else {
-					val -= parseFloat( jQuery.css( elem, "border" + cssExpand[ i ] + "Width" ) ) || 0;
+		if ( val > 0 ) {
+			if ( extra !== "border" ) {
+				for ( ; i < len; i += 2 ) {
+					if ( !extra && !boxSizing ) {
+						val -= parseFloat( jQuery.css( elem, "padding" + cssExpand[ i ] ) ) || 0;
+					}
+					if ( extra === "margin" ) {
+						val += parseFloat( jQuery.css( elem, extra + cssExpand[ i ] ) ) || 0;
+					} else if ( !boxSizing ) {
+						val -= parseFloat( jQuery.css( elem, "border" + cssExpand[ i ] + "Width" ) ) || 0;
+					}
 				}
 			}
-		}
 
-		return val + "px";
-	}
+			return val + "px";
+		}
 
 	// Fall back to computed then uncomputed css if necessary
 	val = curCSS( elem, name, name );
